99e73bd203b56bbd52047cc86356eedd6b115538
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2019-05-25 19:19:23 +0530

    Add router for multiple pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Login from "./Components/Login.js";
 
 import "./App.css";
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Route path="/" exact component={Login} />
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import Form from "./base-components/Form.js";
 import Input from "./base-components/Input.js";
